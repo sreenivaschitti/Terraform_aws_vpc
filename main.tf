32dbae65ac74d3461a145603cd643d7612852518
sreenivas <sreenivaschitti@gmail.com>
2026-03-04 06:11:53 +0530

    vpc

--- main.tf
+++ main.tf
@@ -9,19 +9,10 @@ resource "aws_vpc" "main" {
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name = "main"
-  }
+  tags = local.igw_final_tags
 }
 
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main"
-  }
-}
 
 
 
