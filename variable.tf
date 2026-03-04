2c90498679d2509180792b8b75c85d33da5351e4
sreenivas <sreenivaschitti@gmail.com>
2026-03-04 06:45:41 +0530

    vpc

--- variable.tf
+++ variable.tf
@@ -24,9 +24,9 @@ variable "igw_tags"{
     default = {}
     }    
 
-variable "subnet_cidr"{
-    type = list(string)
+variable "public_subnet_cidr"{
+    type = list
 
-    default = { "10.0.1.0/24" , "10.0.2.0/24"}
+    default = ["10.0.1.0/24" , "10.0.2.0/24"]
 
 }    
