79b402dc86f4c5a5639d33d95d688de6daa4fc10
sreenivas <sreenivaschitti@gmail.com>
2026-03-04 05:33:43 +0530

    vpc

--- locals.tf
+++ locals.tf
@@ -4,18 +4,18 @@ common_tags  = {
     
        project = var.project
     environment = var.environment
-    Terraform = true
+    Terraform = "true"
 
 }
 
-vpc_final_tags = merge { 
+vpc_final_tags = merge( 
     
         local.common_tags,
 
         Name = "${var.project} - ${var.environment}",
 
         var.vpc_tags
-  }
+)
 
 
 
