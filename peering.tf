cd00c43d77e554f07e5da03cc247492366dff22a
sreenivas <sreenivaschitti@gmail.com>
2026-03-18 21:23:29 +0530

    vpc

--- peering.tf
+++ peering.tf
@@ -28,12 +28,12 @@ resource "aws_route" "public_peering" {
   count = var.is_peering_required ? 1 : 0  
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
 }
 
 resource "aws_route" "default_peering" {
   count = var.is_peering_required ? 1 : 0  
   route_table_id            = data.aws_vpc.default.main_route_table_id
   destination_cidr_block    = var.vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
 }
