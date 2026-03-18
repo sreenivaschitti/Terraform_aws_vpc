375fd908e98b6c2de25248a44f4089e835382d54
sreenivas <sreenivaschitti@gmail.com>
2026-03-18 21:20:49 +0530

    vpc

--- peering.tf
+++ peering.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_peering_connection" "main" {
   count = var.is_peering_required ? 1 : 0
-  peer_vpc_id   = data.aws_vpc.default_vpc.id
+  peer_vpc_id   = data.aws_vpc.default.id
   vpc_id        = aws_vpc.main.id
 
   accepter {
