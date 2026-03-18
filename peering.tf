a43ab1bfbf6beac930b82b7bc382ff372fbba4a3
sreenivas <sreenivaschitti@gmail.com>
2026-03-18 21:55:43 +0530

    vpc

--- peering.tf
+++ peering.tf
@@ -31,6 +31,20 @@ resource "aws_route" "public_peering" {
   vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
 }
 
+resource "aws_route" "private_peering" {
+  count = var.is_peering_required ? 1 : 0  
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
+}
+
+resource "aws_route" "database_peering" {
+  count = var.is_peering_required ? 1 : 0  
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
+}
+
 resource "aws_route" "default_peering" {
   count = var.is_peering_required ? 1 : 0  
   route_table_id            = data.aws_vpc.default.main_route_table_id
