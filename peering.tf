ef65e1d52dc0982a7163c1c0771f64c8abb053d5
sreenivas <sreenivaschitti@gmail.com>
2026-03-18 22:15:32 +0530

    vpc

--- peering.tf
+++ peering.tf
@@ -2,7 +2,7 @@ resource "aws_vpc_peering_connection" "main" {
   count = var.is_peering_required ? 1 : 0
   peer_vpc_id   = data.aws_vpc.default.id
   vpc_id        = aws_vpc.main.id
-
+    auto_accept = true
   accepter {
     allow_remote_vpc_dns_resolution = true
   }
